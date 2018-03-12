2584d66a38dc743444ae9bf2aaf8920ab1f976dd
Lohou Anthony <anthony.lohou@laposte.net>
2018-03-12 09:58:06 +0100

    Selection

---
+++ src/model/Selection.java
@@ -0,0 +1,50 @@
+public class Selection{
+
+  private int beginning;
+  private int length;
+
+
+  /**
+  *
+  * @param b Beginning Index of the Selection
+  * @param l length of the Selection
+  **/
+  public Selection(int b, int l){
+    beginning = b;
+    length = l;
+  }
+
+  /**
+  * Access to the beginning index
+  *
+  * @return int  Index of the beginning
+  **/
+  public int getBeginning(){
+    return this.beginning;
+  }
+  /**
+  * Access to the beginning index
+  *
+  * @param b  Index of the beginning
+  **/
+  public void setBeginning(int b){
+    this.beginning = b;
+  }
+
+  /**
+  * Access to the length
+  *
+  * @return int  Length of the selection
+  **/
+  public int getLength(){
+    return this.length;
+  }
+  /**
+  * Access to the beginning index
+  *
+  * @param l  length of the selection
+  **/
+  public void setLength(int l){
+    if(l>0){this.legth = l;}
+  }
+}
