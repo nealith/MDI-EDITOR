820ca4960efed05bbd656ea49adafba0e16d9e33
nealith <nealith@neaworld.fr>
2018-04-22 15:11:20 +0200

    implement undo / redo

--- src/controller/Editor.java
+++ src/controller/Editor.java
@@ -11,10 +11,15 @@ public class Editor{
   private Selection selection;
   static private Editor editeur = null;
 
+  private Stack<Memento> undos;
+  private Stack<Memento> redos;
+
   private Editor(){
     main_window = UI.getInstance();
     texte = Text.getInstance();
     selection = new Selection(0,0);
+    undos = new Stack<Memento>();
+    redos = new Stack<Memento>();
   }
 
   static public Editor getInstance(){
@@ -42,6 +47,30 @@ public class Editor{
 
   public static void main(String[] args) {
     Editor e = Editor.getInstance();
+  }
+
+  public void undo(){
+    if (!undos.empty()) {
+      Memento u = undos.pop();
+      Text.getInstance().restore(u);
+      redos.push(u);
+    }
+  }
+
+  public void redo(){
+    if (!redos.empty()) {
+      Memento u = redos.pop();
+      Text.getInstance().restore(u);
+      undos.push(u);
+    }
+  }
+
+  public void canUndo(){
+    return !undos.empty();
+  }
+
+  public void canRedo(){
+    return !redos.empty();
   }
 
 }
